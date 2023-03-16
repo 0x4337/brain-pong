1d9c530e06c9cc97cca16a2c90f5517cc61bc6f1
Vantria <s1spam118@gmail.com>
2023-03-16 15:26:04 +0000

    add hero

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,7 +1,27 @@
 import "./HomePage.scss";
 
 const HomePage = () => {
-  return <> </>;
+  return (
+    <main className="hero">
+      <section className="hero__div">
+        <h1 className="hero__title">
+          BrainP<span className="hero__title-ball">o</span>ng
+        </h1>
+        <p className="hero__subtitle">BrainStations PingPong Leaderboard.</p>
+      </section>
+
+      <section className="input">
+        <h4 className="input__title">Input Your Name</h4>
+
+        <div className="input__wrapper">
+          <form className="input__form">
+            <input placeholder="name" type="text" className="input__input" />
+            <button className="input__submit">Submit</button>
+          </form>
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default HomePage;
