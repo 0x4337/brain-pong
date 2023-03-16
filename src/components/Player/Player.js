c659269d2f7699e159fa2edf7ac0a0cff6bdb321
Vantria <s1spam118@gmail.com>
2023-03-16 20:26:39 +0000

    add search highlighting

---
+++ src/components/Player/Player.js
@@ -0,0 +1,142 @@
+import "./Player.scss";
+
+const Player = ({ player, playerId }) => {
+  console.log(player);
+  const winRate = (player.gamesWon / player.gamesPlayed) * 100;
+  const isHighlighted = player.id === playerId;
+
+  const getClassName = (baseClass) => {
+    return `${baseClass}${isHighlighted ? ` ${baseClass}--highlighted` : ""}`;
+  };
+
+  return (
+    <article className={getClassName("player")}>
+      <div className={getClassName("player__div")}>
+        <p className={getClassName("player__name")}>{player.name}</p>
+        <p className={getClassName("player__wins")}>{player.gamesWon}</p>
+        <p className={getClassName("player__played")}>{player.gamesPlayed}</p>
+        <p className={getClassName("player__losses")}>
+          {player.gamesPlayed - player.gamesWon}
+        </p>
+        <p className={getClassName("player__winrate")}>
+          {Math.floor(winRate)}%
+        </p>
+      </div>
+    </article>
+  );
+};
+
+export default Player;
+
+// import "./Player.scss";
+
+// const Player = ({ player, playerId }) => {
+//   console.log(player);
+//   const winRate = (player.gamesWon / player.gamesPlayed) * 100;
+//   const isHighlighted = player.id === playerId;
+
+//   return (
+//     <article className={`player${isHighlighted ? " player--highlighted" : ""}`}>
+//       <div
+//         className={`player__div${
+//           isHighlighted ? " player__div--highlighted" : ""
+//         }`}
+//       >
+//         <p
+//           className={`player__name${
+//             isHighlighted ? " player__name--highlighted" : ""
+//           }`}
+//         >
+//           {player.name}
+//         </p>
+//         <p
+//           className={`player__wins${
+//             isHighlighted ? " player__wins--highlighted" : ""
+//           }`}
+//         >
+//           {player.gamesWon}
+//         </p>
+//         <p
+//           className={`player__played${
+//             isHighlighted ? " player__played--highlighted" : ""
+//           }`}
+//         >
+//           {player.gamesPlayed}
+//         </p>
+//         <p
+//           className={`player__losses${
+//             isHighlighted ? " player__losses--highlighted" : ""
+//           }`}
+//         >
+//           {player.gamesPlayed - player.gamesWon}
+//         </p>
+//         <p
+//           className={`player__winrate${
+//             isHighlighted ? " player__winrate--highlighted" : ""
+//           }`}
+//         >
+//           {winRate.toFixed(2)}%
+//         </p>
+//       </div>
+//     </article>
+//   );
+// };
+
+// export default Player;
+
+// import "./Player.scss";
+
+// const Player = ({ player, playerId }) => {
+//   console.log(player);
+//   const winRate = (player.gamesWon / player.gamesPlayed) * 100;
+
+//   //   if (player.id === playerId) {
+//   //     return <Player player={player} className="leaderboard__selected" />;
+//   //   } else {
+//   //     return <Player player={player} />;
+//   //   }
+
+//   const createPlayerNormal = () => {
+//     return (
+//       <article className="player">
+//         <div className="player__div">
+//           <p className="player__name">{player.name}</p>
+//           <p className="player__wins">{player.gamesWon}</p>
+//           <p className="player__played">{player.gamesPlayed}</p>
+//           <p className="player__losses">
+//             {player.gamesPlayed - player.gamesWon}
+//           </p>
+//           <p className="player__winrate">{winRate}%</p>
+//         </div>
+//       </article>
+//     );
+//   };
+
+//   const createPlaterHighlighted = () => {
+//     return (
+//       <article className="player player--highlighted">
+//         <div className="player__div">
+//           <p className="player__name">{player.name}</p>
+//           <p className="player__wins">{player.gamesWon}</p>
+//           <p className="player__played">{player.gamesPlayed}</p>
+//           <p className="player__losses">
+//             {player.gamesPlayed - player.gamesWon}
+//           </p>
+//           <p className="player__winrate">{winRate}%</p>
+//         </div>
+//       </article>
+//     );
+//   };
+
+//   const createPlayter = () => {
+//     if (player.id === playerId) {
+//       return createPlaterHighlighted();
+//     } else {
+//       return createPlayerNormal();
+//     }
+//   };
+
+//   return createPlayter();
+// };
+
+// export default Player;
--- src/pages/Leaderboard/Leaderboard.js
+++ src/pages/Leaderboard/Leaderboard.js
@@ -1,7 +1,32 @@
 import "./Leaderboard.scss";
+import Player from "../../components/Player/Player";
+import { useParams } from "react-router-dom";
 
-const Leaderboard = () => {
-  return <></>;
+const Leaderboard = ({ players }) => {
+  const { playerId } = useParams();
+
+  // TODO:
+  // If the player ID matches the player ID in the URL, add a class to the player component
+  // that will highlight the player in the leaderboard.
+
+  return (
+    <section className="leaderboard">
+      <h2 className="leaderboard__title">Leaderboard</h2>
+
+      <div className="leaderboard__players">
+        <div className="leaderboard__wrapper">
+          <p className="leaderboard__stat">Name</p>
+          <p className="leaderboard__stat">Wins</p>
+          <p className="leaderboard__stat">Played</p>
+          <p className="leaderboard__stat">Losses</p>
+          <p className="leaderboard__stat">Winrate</p>
+        </div>
+        {players.map((player) => {
+          return <Player player={player} playerId={playerId} />;
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Leaderboard;
